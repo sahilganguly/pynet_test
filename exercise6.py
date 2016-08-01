31d0f5fee7331e2e22da36e64541f4550b5121ed
Sahil Ganguly <sganguly@box.com>
2016-08-01 14:51:11 -0700

    Exercise 6

---
+++ exercise6.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+nu1 = raw_input("Please enter the first number: ")
+nu2 = raw_input("Please enter the second number: ")
+
+print "\nSum = {}".format(int(nu1)+int(nu2))
+print "Difference = {}".format(int(nu1)-int(nu2))
+print "Product = {}".format(int(nu1)*int(nu2))
+print "Remainder = {}".format(int(nu1)%float(nu2))
